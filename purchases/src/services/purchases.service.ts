af99a4f2cd0fb2283acda612c476d8fea960142b
Daniel Hessel <daniel@sharbe.com.br>
2022-04-12 17:42:56 -0300

    feat: creating purchases and getting logged user

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface CreatePurchaseParams {
+  customerId: string;
+  productId: string;
+}
+
 @Injectable()
 export class PurchasesService {
   // eslint-disable-next-line prettier/prettier
@@ -10,6 +15,25 @@ export class PurchasesService {
     return this.prisma.purchase.findMany({
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ customerId, productId }: CreatePurchaseParams) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found.');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customerId,
+        productId,
       },
     });
   }
