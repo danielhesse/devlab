f3167e2d9397225d297743f8d995d18140fba03d
Daniel Hessel <daniel@sharbe.com.br>
2022-04-23 11:06:13 -0300

    web:feat: finalized the communication setup with graphql

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,19 +1,24 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { useGetProductsQuery } from "../../graphql/generated/graphql";
+import { useGetProductsQuery, useMeQuery } from "../../graphql/generated/graphql";
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
 import { withApollo } from "../../lib/withApollo";
 
-export function Home() {
+export function Home({ data }) {
   const { user } = useUser();
 
-  const { data, loading, error } = useGetProductsQuery();
+  const { data: me } = useMeQuery();
 
   return (
     <div>
       <h1>Hello World</h1>
 
       <pre>
-        {JSON.stringify(data, null, 2)}
+        {JSON.stringify(me, null, 2)}
       </pre>
+
+      {/* <pre>
+        {JSON.stringify(data.products, null, 2)}
+      </pre> */}
 
       <pre>
         {JSON.stringify(user, null, 2)}
@@ -23,11 +28,15 @@ export function Home() {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
+  getServerSideProps: async (ctx) => {
+    // getServerPageGetProducts({}, ctx);
+
     return {
       props: {}
     };
   }
 });
 
-export default withApollo(Home);
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
